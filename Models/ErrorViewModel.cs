dac05c6fd864ea342b18d5c2d929921f3f503f04
k4yr2 <kyrzkaya@gmail.com>
2024-12-20 18:21:36 +0300

    Project prepared

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace muhammedkayraozkaya_241103046.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
