476ad17e811b64287a7329eecdf8c501d1265510
k4yr2 <kyrzkaya@gmail.com>
2024-12-21 21:32:38 +0300

    Base and book models

---
+++ Models/BaseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace muhammedkayraozkaya_241103046.Models
+{
+    abstract public class BaseModel
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+    }
+}
---
+++ Models/BookModel.cs
@@ -0,0 +1,18 @@
+﻿namespace muhammedkayraozkaya_241103046.Models
+{
+    public class BookModel : BaseModel
+    {
+        public int AuthorId { get; set; }
+
+        public BookCategory Category { get; set; }
+    }
+
+    public enum BookCategory
+    {
+        Unknown,
+
+        History,
+
+        Philosophy,
+    }
+}
