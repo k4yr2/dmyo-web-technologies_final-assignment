79f7c573415048914ffb9d3ac28e502341a33d99
k4yr2 <kyrzkaya@gmail.com>
2024-12-22 03:52:16 +0300

    Author model created

---
+++ Models/AuthorModel.cs
@@ -0,0 +1,42 @@
+﻿namespace muhammedkayraozkaya_241103046.Models
+{
+    public class AuthorModel : BaseModel
+    {
+        public AuthorName Name { get; } = new();
+
+        public AuthorDate Date { get; } = new();
+    }
+
+    public class AuthorName
+    {
+        public string? First { get; set; }
+
+        public string? Last { get; set; }
+
+        public string? GetFull()
+        {
+            var flag = First == null;
+            var flag2 = Last == null;
+
+            if (flag)
+            {
+                if (flag2)
+                    return null;
+
+                return Last;
+            }
+
+            if (flag2)
+                return First;
+
+            return $"{First} {Last}";
+        }
+    }
+
+    public class AuthorDate
+    {
+        public DateTime? Birth { get; set; }
+
+        public DateTime? Death { get; set; }
+    }
+}
