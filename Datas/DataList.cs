1f50581974a70e0707bafb1ce4eae9241dbed6e9
k4yr2 <kyrzkaya@gmail.com>
2024-12-22 13:20:35 +0300

    data list added

---
+++ Datas/DataList.cs
@@ -0,0 +1,10 @@
+﻿using muhammedkayraozkaya_241103046.Models;
+
+namespace muhammedkayraozkaya_241103046.Datas
+{
+    public class DataList<TModel> where TModel : BaseModel
+    {
+        readonly Dictionary<int, TModel> cache = new();
+
+    }
+}
