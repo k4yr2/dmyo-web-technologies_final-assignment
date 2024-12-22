f1a41d8691c586ed1c43f7f79d3e7242cd6ccaaa
k4yr2 <kyrzkaya@gmail.com>
2024-12-22 14:23:31 +0300

    basic central datas

--- Datas/DataStore.cs
+++ Datas/DataStore.cs
@@ -4,6 +4,10 @@ namespace muhammedkayraozkaya_241103046.Datas
 {
     static public class DataStore
     {
+        static public readonly DataList<GenreModel> genres = new();
 
+        static public readonly DataList<AuthorModel> authors = new();
+
+        static public readonly DataList<BookModel> books = new();
     }
 }
