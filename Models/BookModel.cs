627bd8cb4dbc6c9c4ed85a493f6bb9e126e3c103
k4yr2 <kyrzkaya@gmail.com>
2024-12-22 12:33:41 +0300

    genre and author id changed as read-only

--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -1,17 +1,17 @@
 ﻿namespace muhammedkayraozkaya_241103046.Models
 {
-    public class BookModel(int id) : BaseModel(id)
+    public class BookModel(int id, int genre, int author) : BaseModel(id)
     {
+        public int GenreId { get; } = genre;
+
+        public int AuthorId { get; } = author;
+
         public string? Title { get; set; }
 
         public string? Description { get; set; }
 
-        public int GenreId { get; set; }
+        public int StockCount { get; }
 
-        public int AuthorId { get; set; }
-
-        public int StockCount { get; set; }
-
-        public int Rating { get; set; }
+        public int Rating { get; }
     }
 }
