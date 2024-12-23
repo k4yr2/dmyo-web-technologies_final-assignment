80d73c9869e6c60fd67958b8a6a4e2633b918fd5
k4yr2 <kyrzkaya@gmail.com>
2024-12-23 13:28:47 +0300

    end points for books

---
+++ Controllers/Api/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using muhammedkayraozkaya_241103046.Datas;
+using muhammedkayraozkaya_241103046.Models;
+
+namespace muhammedkayraozkaya_241103046.Controllers.Api
+{
+    [ApiController, Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            return Ok(DataStore.books.Models); // JSON olarak döndür
+        }
+    }
+}
