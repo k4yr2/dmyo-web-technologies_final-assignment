f7b4757fca7add1e1f01f58e34d75f782123db80
k4yr2 <kyrzkaya@gmail.com>
2024-12-23 13:34:57 +0300

    end points for authors

---
+++ Controllers/Api/AuthorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using muhammedkayraozkaya_241103046.Datas;
+using muhammedkayraozkaya_241103046.Models;
+
+namespace muhammedkayraozkaya_241103046.Controllers.Api
+{
+    [ApiController, Route("api/[controller]")]
+    public class AuthorsController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllAuthors()
+        {
+            return Ok(DataStore.authors.Models); // JSON olarak döndür
+        }
+    }
+}
