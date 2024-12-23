de778e2c991052f1f8c9d0bb40b80e75278ab173
k4yr2 <kyrzkaya@gmail.com>
2024-12-24 00:32:29 +0300

    utility class for enumerable books

---
+++ Utils/EnumerableBooks.cs
@@ -0,0 +1,32 @@
+﻿using muhammedkayraozkaya_241103046.Datas;
+using muhammedkayraozkaya_241103046.Models;
+
+namespace muhammedkayraozkaya_241103046.Utils
+{
+    static public class EnumerableBooks
+    {
+        static public IEnumerable<BookModel> All
+        {
+            get
+            {
+                return DataStore.books.Models;
+            }
+        }
+
+        static public IEnumerable<BookModel> MostRating
+        {
+            get
+            {
+                return All.OrderBy(b => b.Rating);
+            }
+        }
+
+        static public IEnumerable<BookModel> LestRating
+        {
+            get
+            {
+                return All.OrderByDescending(b => b.Rating);
+            }
+        }
+    }
+}
