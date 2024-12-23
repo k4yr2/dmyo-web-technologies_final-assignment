cc59d107ce4ef91be6266409af3d308de388aa0c
k4yr2 <kyrzkaya@gmail.com>
2024-12-23 13:32:04 +0300

    end points for genres

---
+++ Controllers/Api/GenresControl.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using muhammedkayraozkaya_241103046.Datas;
+using muhammedkayraozkaya_241103046.Models;
+
+namespace muhammedkayraozkaya_241103046.Controllers.Api
+{
+    [ApiController, Route("api/[controller]")]
+    public class GenresController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllGenres()
+        {
+            return Ok(DataStore.genres.Models); // JSON olarak döndür
+        }
+    }
+}
