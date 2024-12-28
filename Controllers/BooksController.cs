c0a6a2f1ce7c749af2c276686cf484362c07ba6a
k4yr2 <kyrzkaya@gmail.com>
2024-12-28 14:12:50 +0300

    added book controller and its views

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using muhammedkayraozkaya_241103046.Datas;
+
+namespace muhammedkayraozkaya_241103046.Controllers
+{
+    [Route("books")]
+    public class BooksController : Controller
+    {
+        [HttpGet, Route("")]
+        public IActionResult Index()
+        {
+            return View(DataStore.books.Models());
+        }
+
+        [HttpGet, Route("details")]
+        public IActionResult Details(int id)
+        {
+            return View(DataStore.books.Model(id)); 
+        }
+    }
+}
