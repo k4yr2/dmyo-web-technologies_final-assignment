c7095629947e8c4aecd0ee843abac7692fe2ec07
k4yr2 <kyrzkaya@gmail.com>
2024-12-30 00:36:20 +0300

    added authors controller and views

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using muhammedkayraozkaya_241103046.Datas;
+
+namespace muhammedkayraozkaya_241103046.Controllers
+{
+	[Route("authors")]
+	public class AuthorsController : Controller
+	{
+		[HttpGet, Route("")]
+		public IActionResult Index()
+		{
+			return View(/*DataStore.authors.Models()*/);
+		}
+
+		[HttpGet, Route("details")]
+		public IActionResult Details(int id)
+		{
+			return View(/*DataStore.authors.Model(id)*/);
+		}
+	}
+}
