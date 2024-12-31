7ae24b92f2ef88532d98f3dd2463f5ba1e472c75
k4yr2 <kyrzkaya@gmail.com>
2024-12-31 18:39:27 +0300

    account controller added

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace muhammedkayraozkaya_241103046.Controllers
+{
+	[Route("account")]
+	public class AccountController : Controller
+	{
+		[HttpGet, Route("login")]
+		public IActionResult Login()
+		{
+			return View();
+		}
+	}
+}
