271ce10d621f538db12423f52dcf6643bc5e7260
k4yr2 <kyrzkaya@gmail.com>
2024-12-31 18:52:14 +0300

    added role

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,9 +1,11 @@
 ﻿namespace muhammedkayraozkaya_241103046.Models
 {
-	public class UserModel(int id, string username, string password) : BaseModel(id)
+	public class UserModel(int id, string username, string password, string role) : BaseModel(id)
 	{
 		public string Username { get; set; } = username;
 
 		public string Password { get; set; } = password;
-	}
+
+        public string Role { get; set; } = role;
+    }
 }
