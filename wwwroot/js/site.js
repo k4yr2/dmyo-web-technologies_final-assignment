e73c3e7983d71ed507242db1773c663ed56dfb42
k4yr2 <kyrzkaya@gmail.com>
2024-12-31 21:51:38 +0300

    learned ajax like tf

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -20,3 +20,26 @@
 setInterval(() => {
     updateCards();
 }, 0);
+
+$(document).ready(function () {
+    // Artı butonuna tıklama olayını dinliyoruz
+    $('.book-card .cover .info .add').on('click', function () {
+        var bookId = $(this).data('book-id'); // Butondaki kitap ID'sini alıyoruz
+
+        $.ajax({
+            url: '/account/add-book',
+            type: 'POST',
+            data: { id: bookId },
+            success: function (response) {
+                if (response.success) {
+                    alert(response.message); // Başarılı ekleme mesajı
+                } else {
+                    alert(response.message); // Kitap zaten favorilerde
+                }
+            },
+            error: function () {
+                alert('An error occurred while adding to favorites.');
+            }
+        });
+    });
+});
